5fe5c55b4df57cdff14e4174c64edf58f6719243
Kichirou <mrphuc244@gmail.com>
2024-04-03 11:55:14 +0700

    update Buoi7

--- Buoi7/SQL/Execute.sql
+++ Buoi7/SQL/Execute.sql
@@ -5,10 +5,8 @@ SELECT	* FROM	NhanVien AS nv WHERE  nv.hoten LIKE 'Le%'
 -- Tinh TBC cuar HSL
 SELECT	AVG(nv.hsl) FROM	NhanVien AS nv 
 -- Them vao 1 nhan vien
-INSERT INTO NhanVien ('nv5', 'Thanh Nhan', 1, VALUES(CONVERT(date, 'yyyy-MM-dd', '1999-02-02')) , 5, 'e@husc.edu.vn', 5)
+INSERT INTO NhanVien VALUES('nv6', 'A', 0, '2000-05-05', 4, 'd@husc.edu.vn', 4)
 -- Tang HSL len 2 cho nhanvien co manv = 'nv1'
-UPDATE NhanVien
-SET hsl = 2
-WHERE manv = 'nv1'
+UPDATE NhanVien SET hsl = 2 WHERE manv = 'nv1'
 -- Xoa nhan vien nv1
 DELETE FROM NhanVien WHERE manv='nv1'
